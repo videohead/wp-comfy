5af74c7f2e194c96f9de40245974af98527d46e3
Matthew Galvin <videohead@hotmail.com>
2026-07-16 15:59:58 -0700

    Modify wp-config.php for database settings
    Updated database configuration settings and authentication keys.

--- wp-data/wp-config.php
+++ wp-data/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'activema_wpcomfy' );
+define( 'DB_NAME', 'wpcomfy' );
 
 /** Database username */
-define( 'DB_USER', 'activema_matthew' );
+define( 'DB_USER', '' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'dcrvx1000' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'db' );
